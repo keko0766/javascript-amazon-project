ab7d5492ec482be6ca9e3dd086831999d84a9e46
keko <qoyshibekkerei@gmail.com>
2025-09-13 18:24:47 +0200

    Make the kart quatnity interactive

--- scripts/amazon.js
+++ scripts/amazon.js
@@ -1,6 +1,15 @@
 let productsHTML = ``
+const quantityElem = document.querySelector('.cart-quantity')
 
-products.forEach((product) => {
+function quantityUpdate(){ //Update item quantity
+  let quantity = 0
+  cart.forEach((item) => {
+    quantity += item.quantity
+  })
+  quantityElem.innerHTML = quantity
+}
+
+products.forEach((product) => { // Adds a product block from json
   productsHTML += `
           <div class="product-container">
             <div class="product-image-container">
@@ -46,7 +55,7 @@ products.forEach((product) => {
               Added
             </div>
 
-            <button class="add-to-cart-button button-primary">
+            <button class="add-to-cart-button button-primary" data-product-id="${product.id}">
               Add to Cart
             </button>
           </div>
@@ -55,3 +64,39 @@ products.forEach((product) => {
  
 document.querySelector('.products-grid')
   .innerHTML = productsHTML
+
+document.querySelectorAll('[data-product-id]') //Adds a product to cart onclick
+  .forEach((button) => {
+    button.addEventListener('click', () => {
+      const productId = button.dataset.productId
+
+      let matchingItem;
+
+      cart.forEach((item) => {
+        if(item.productId === productId){
+          matchingItem = item
+        }
+      })
+
+      if(matchingItem){
+        matchingItem.quantity++
+      } else{
+        cart.push({
+          productId: productId,
+          quantity: 1
+        })
+      }
+      quantityUpdate()
+  })
+})
+
+
+
+function showCart(){
+  setTimeout(() => {
+    document.querySelector('.added-to-cart')
+      .forEach((item) => {
+
+      })
+  },)
+}
